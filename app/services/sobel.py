e65f4b251f61cb43f048867bda967e73cac05aa6
Guilherme Nepomuceno de Carvalho <guilhermenepomuceno46@gmail.com>
2021-12-11 18:44:46 -0300

    Correção Normalização sobel

--- app/services/sobel.py
+++ app/services/sobel.py
@@ -1,7 +1,6 @@
 """."""
-from .convolucao import convolucao
+from .convolucao import convolucao, normalizaImagem
 import numpy as np
-import cv2
 
 
 def normalizar(img):
@@ -12,7 +11,7 @@ def normalizar(img):
 
 def sobel(imagem, bordas, normaliza):
     """."""
-    imagem_original = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    normaliza = 1 if normaliza == 0 else 0
     if bordas == 0:
         imagem = convolucao(
             imagem,
@@ -34,52 +33,20 @@ def sobel(imagem, bordas, normaliza):
             normaliza
         )
     elif bordas == 2:
-        imagem = np.abs(convolucao(
+        imagem = convolucao(
             imagem,
             np.array([
                 [-1, 0, 1],
                 [-2, 0, 2],
                 [-1, 0, 1],
             ]),
-            normaliza)) + \
-            np.abs(convolucao(
+            normaliza) + convolucao(
                 imagem,
                 np.array([
                     [-1, -2, -1],
                     [0, 0, 0],
                     [1, 2, 1],
                 ]),
-                normaliza
-            )) if normaliza == 0 else convolucao(
-                imagem,
-                np.array([
-                    [-1, 0, 1],
-                    [-2, 0, 2],
-                    [-1, 0, 1],
-                ]),
-                normaliza) + convolucao(
-                    imagem,
-                    np.array([
-                        [-1, -2, -1],
-                        [0, 0, 0],
-                        [1, 2, 1],
-                    ]),
-                    normaliza)
-    elif bordas == 5:
-        imagem = imagem_original + convolucao(
-            imagem,
-            np.array([
-                [1, 1, 1],
-                [1, -8, 1],
-                [1, 1, 1],
-            ]))
-    else:
-        highBoost = imagem_original - convolucao(
-            imagem,
-            np.array([
-                [1, 2, 1],
-                [2, 4, 2],
-                [1, 2, 1],
-            ]) / 16)
-        imagem = imagem_original + 1.5 * highBoost
-    return normalizar(imagem) if normaliza > 0 else imagem
+                normaliza)
+        imagem = normalizaImagem(imagem) * 255 if normaliza == 0 else imagem
+    return imagem if normaliza == 0 else imagem * 255
